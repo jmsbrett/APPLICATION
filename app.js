ff2fb5dacee337cd1fee3a55daae11efb4ae7f9b
James Brett <jmsbrett@gmail.com>
2018-03-19 12:25:30 -0500

    changes

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.render("index.html")
